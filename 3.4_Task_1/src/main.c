f59260241e11e41474ade3503b2f0753c81cc469
Raudeck <70005949+Raudeck@users.noreply.github.com>
2022-10-26 22:12:23 +0800

    Finish 3.4

--- 3.4_Task_1/src/main.c
+++ 3.4_Task_1/src/main.c
@@ -26,6 +26,66 @@ void push(struct Node **node, int data)
     }
 }
 
+void swap(struct Node **node, int x, int y)
+{
+    struct Node *prevX = NULL, *currX = NULL;
+    currX = *node;
+
+    // Search for X
+    while (currX && currX->data != x)
+    {
+        prevX = currX;
+        currX = currX->next;
+    }
+
+    struct Node *prevY = NULL, *currY = NULL;
+    currY = *node;
+    // Search for Y
+    while(currY && currY->data != y)
+    {
+        prevY = currY;
+        currY = currY->next;
+    }
+
+    // If either x or y is not found, nothing to do
+    if (currX == NULL || currY == NULL)
+        return ;
+    
+    prevX->next = currY;
+    prevY->next = currX;
+
+    struct Node *temp = currY->next;
+    currY->next = currX->next;
+    currX->next = temp;
+}
+
+void reverse(struct Node **node)
+{
+    struct Node *prev = NULL, *curr = NULL, *next = NULL;
+    
+    curr = *node;
+
+    while (curr)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    *node = prev;
+}
+
+void print(struct Node *node)
+{
+    while (node)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    putchar('\n');
+}
+
 int main()
 {
     struct Node *node = NULL;
@@ -35,14 +95,11 @@ int main()
     push(&node, 9);
     push(&node, 15);
     push(&node, 5);
-
-    while (node)
-    {
-        printf("%d ", node->data);
-        node = node->next;
-    }
-    putchar('\n');
-
+    print(node);
+    swap(&node, 20, 9);
+    print(node);
+    reverse(&node);
+    print(node);
     system("pause");
     return 0;
 }
